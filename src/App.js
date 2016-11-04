5366e8d6fd0411dd6e0faacea7dbcf23111ce36e
Daniel Gamage <hello@danielgamage.com>
2016-11-04 04:39:26 -0400

    load font capability

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+
+  }
+  readFile(e) {
+    // should probably cache if font already exists
+    [...e.target.files].map((file, i) => {
+      let reader = new FileReader()
+      reader.addEventListener("load", () => {
+        var bitterFontFace = new FontFace(file.name, reader.result)
+        console.log(bitterFontFace)
+        document.fonts.add(bitterFontFace)
+      }, false)
+      reader.readAsArrayBuffer(file)
+      return true
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -11,6 +27,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
+          <input id="lol" type="file" onChange={this.readFile} />
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>

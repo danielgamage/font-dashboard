0f8ad891e1df9a5f2a9389d1ad4f140e19d68aad
Daniel Gamage <hello@danielgamage.com>
2016-11-15 17:51:58 -0500

    basic fullscreen support

--- src/components/ActionBar.js
+++ src/components/ActionBar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { ActionCreators } from 'redux-undo'
+import fullscreenIcon from '../icons/fullscreen.svg'
 
 import TextSamples from './TextSamples'
 
@@ -17,48 +18,57 @@ class ActionBar extends Component {
     const dispatch = this.props.dispatch
     return (
       <div className='ActionBar'>
-        <button
-          className='DeleteButton'
-          onClick={() => {
-            dispatch({
-              type: 'DELETE_TEXTBOX'
-            })
-          }}>
-          Delete
-        </button>
-        <button
-          className={'TextSamplesButton' + (this.state.samplesOpen ? ' open' : '')}
-          onClick={() => {
-            this.setState({
-              samplesOpen: !this.state.samplesOpen
-            })
-          }}>
-          Abc...
-        </button>
-        { this.state.samplesOpen && <TextSamples/> }
-        <button
-          onClick={() => {
-            dispatch(ActionCreators.undo()) // undo the last action
-          }}>
-          Undo
-        </button>
-        <button
-          onClick={() => {
-            dispatch(ActionCreators.redo()) // undo the last action
-          }}>
-          Redo
-        </button>
-        <button
-          onClick={() => {
-            dispatch({
-              type: 'TOGGLE_FULLSCREEN'
-            })
-          }}>
-          Full
-        </button>
+        <div className='left'>
+          <button
+            className='DeleteButton'
+            onClick={() => {
+              dispatch({
+                type: 'DELETE_TEXTBOX'
+              })
+            }}>
+            Delete
+          </button>
+          <button
+            className={'TextSamplesButton' + (this.state.samplesOpen ? ' open' : '')}
+            onClick={() => {
+              this.setState({
+                samplesOpen: !this.state.samplesOpen
+              })
+            }}>
+            Abc...
+          </button>
+          { this.state.samplesOpen && <TextSamples/> }
+          <button
+            onClick={() => {
+              dispatch(ActionCreators.undo()) // undo the last action
+            }}>
+            Undo
+          </button>
+          <button
+            onClick={() => {
+              dispatch(ActionCreators.redo()) // undo the last action
+            }}>
+            Redo
+          </button>
+        </div>
+        <div className='right'>
+          <button
+            onClick={() => {
+              dispatch({
+                type: 'TOGGLE_FULLSCREEN'
+              })
+            }}>
+            <svg viewBox='0 0 16 16'>
+              <use xlinkHref={fullscreenIcon + `#${this.props.view.fullscreen ? 'exit' : 'enter'}`}></use>
+            </svg>
+          </button>
+        </div>
       </div>
     )
   }
 }
+const mapStateToProps = (state, ownProps) => {
+  return { view: state.view.present }
+}
 
-export default connect()(ActionBar)
+export default connect(mapStateToProps)(ActionBar)

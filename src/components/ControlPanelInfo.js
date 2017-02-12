031471f08b14768510623bc7525116bfd471fa71
Daniel Gamage <hello@danielgamage.com>
2017-02-12 04:02:08 -0500

    better conditionals and styling on font info

--- src/components/ControlPanelInfo.js
+++ src/components/ControlPanelInfo.js
@@ -11,26 +11,29 @@ class ControlPanelInfo extends Component {
   render () {
     return (
       <div className='ControlPanelTab'>
-        {this.props.fonts.length > 0 &&
+        { this.props.fonts.length > 0 &&
           this.props.fonts.map(el => (
             <div
               className='Control full'
               key={el.names.fullName.en} >
               <div className='ControlTitle'>{el.names.fullName.en}</div>
               <div
+                style={{
+                  height: `${(el.tables.os2.sTypoAscender - el.tables.os2.sTypoDescender) / el.unitsPerEm * 10}em`
+                }}
                 className='metrics'>
                 {[
-                  {name: 'Ascender', value: el.tables.os2.sTypoAscender},
-                  {name: 'Cap Height', value: el.tables.os2.sCapHeight},
-                  {name: 'X-Height', value: el.tables.os2.sxHeight},
-                  {name: 'Baseline', value: 0},
-                  {name: 'Descender', value: el.tables.os2.sTypoDescender}
+                  { name: 'Ascender', value: el.tables.os2.sTypoAscender },
+                  { name: 'Cap Height', value: el.tables.os2.sCapHeight },
+                  { name: 'X-Height', value: el.tables.os2.sxHeight },
+                  { name: 'Baseline', value: 0 },
+                  { name: 'Descender', value: el.tables.os2.sTypoDescender }
                 ].map(metric => (
                   <dl
                     key={metric.name}
                     className='metric'
                     style={{
-                    top: `${(-1 * metric.value + el.tables.os2.sTypoAscender) / el.unitsPerEm * 10}em`
+                      top: `${(-1 * metric.value + el.tables.os2.sTypoAscender) / el.unitsPerEm * 10}em`
                   }}>
                     <dt
                       className='metrics__dt'
@@ -43,40 +46,54 @@ class ControlPanelInfo extends Component {
                 <div
                   className='metrics__sample-text'
                   style={{
-                  lineHeight:
-                    el.unitsPerEm > (el.tables.os2.sTypoAscender - el.tables.os2.sTypoDescender)
-                      ? el.tables.os2.sCapHeight / el.unitsPerEm
-                      : (el.tables.os2.sTypoAscender - el.tables.os2.sTypoDescender) / el.unitsPerEm,
-                  fontFamily: el.names.fullName.en
+                    lineHeight: el.unitsPerEm > (el.tables.os2.sTypoAscender - el.tables.os2.sTypoDescender)
+                        ? el.tables.os2.sCapHeight / el.unitsPerEm
+                        : (el.tables.os2.sTypoAscender - el.tables.os2.sTypoDescender) / el.unitsPerEm,
+                    fontFamily: el.names.fullName.en
                 }}>
                   Achg
                 </div>
               </div>
 
-              <dl>
-                <dt>Glyph Count</dt>
-                <dd>{el.numGlyphs}</dd>
-                <dt>UPM</dt>
-                <dd>{el.unitsPerEm}</dd>
-                <dt>Designer</dt>
-                <dd>
-                  <a
-                    href={el.names.designerURL && el.names.designerURL.en}
-                    target='_blank'>
-                    {el.names.designer && el.names.designer.en}
-                  </a>
-                </dd>
-                <dt>Languages</dt>
-                <dd>
+              { el.numGlyphs &&
+                <dl className='font-info'>
+                  <dt>Glyph Count</dt>
+                  <dd>{el.numGlyphs}</dd>
+                </dl>
+              }
+              { el.unitsPerEm &&
+                <dl className='font-info'>
+                  <dt>UPM</dt>
+                  <dd>{el.unitsPerEm}</dd>
+                </dl>
+              }
+              { el.names.designer &&
+                <dl className='font-info'>
+                  <dt>Designer</dt>
+                  <dd>
+                    <a
+                      href={el.names.designerURL && el.names.designerURL.en}
+                      target='_blank'>
+                      {el.names.designer && el.names.designer.en}
+                    </a>
+                  </dd>
+                </dl>
+              }
+              { el.availableLanguages.length > 0 &&
+                <dl className='font-info'>
+                  <dt>Languages</dt>
                   {el.availableLanguages.map((language, i) => (
-                    <span>{language.description}{i < el.availableLanguages.length - 1 && ', '}</span>
+                    <dd>
+                      {language.description}
+                      {i < el.availableLanguages.length - 1 && ', '}
+                    </dd>
                   ))}
-                </dd>
-              </dl>
+                </dl>
+              }
             </div>
           ))
         }
-        {this.props.fonts.length === 0 &&
+        { this.props.fonts.length === 0 &&
           <div className='Control full'>
             <div className='ControlTitle'>No font added</div>
           </div>
